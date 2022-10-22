4921ec1367440edac63bd159a5da522dca7a9192
akaanksha-verma <akaanksha@google.com>
2022-10-22 13:37:15 +0000

    Update Project IDs and Buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "aanya-demo-project-1406-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="aanya-demo-project-1406"
